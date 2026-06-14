9b2dd744046d0043eaa8de13b418985f991e214d
Raphaël Martin <rmartin30210@gmail.com>
2026-06-14 20:11:42 +0200

    fix: fix little features

--- __main__.py
+++ __main__.py
@@ -1,6 +1,7 @@
 from src.model.task_manager import TaskManager
 from src.view.task_view import TaskView
 from src.view.messages import Messages
+from src.view.view_utils import *
 
 
 def main():
@@ -11,43 +12,40 @@ def main():
     print("Welcome to the Terminal Task Manager (TTM) !")
 
     while True:
-        print("\nMenu :")
-        print("1. Add tasks.")
-        print("2. Mark task as complete.")
-        print("3. View all tasks.")
-        print("4. Delete task.")
-        print("5. Exit.")
+        show_menu()
 
         user_choice = int(input("Enter your choice (1-5)"))
 
         match user_choice:
             case 1:
-                print("\n")
+                line_break()
                 title = input("Enter the task title : ")
                 description = input("Enter the task description : ")
                 task_manager.add_task(title, description)
-                task_view.show_message(Messages.TASK_ADDED.format(title=title))
+                show_message(Messages.TASK_ADDED.format(title=title))
 
             case 2:
-                print("\n")
+                line_break()
                 title = input("Enter the task title : ")
                 task_manager.mark_task_complete(title)
-                task_view.show_message(Messages.TASK_COMPLETED.format(title=title))
+                show_message(Messages.TASK_COMPLETED.format(title=title))
             case 3:
-                print("\n")
+                line_break()
                 task_view.show_tasks(task_manager.tasks)
 
             case 4:
-                print("\n")
+                line_break()
                 title = input("Enter the task title : ")
                 task_manager.delete_task(title)
-                task_view.show_message(Messages.TASK_REMOVED.format(title=title))
+                show_message(Messages.TASK_REMOVED.format(title=title))
 
             case 5:
+                line_break()
                 print("Goodbye ! ")
                 return
 
             case _:
+                line_break()
                 print("Enter a valid number")
 
 
---
+++ src/view/view_utils.py
@@ -0,0 +1,15 @@
+def show_message(message):
+    print(message)
+
+
+def show_menu():
+    print("\nMenu :")
+    print("1. Add tasks.")
+    print("2. Mark task as complete.")
+    print("3. View all tasks.")
+    print("4. Delete task.")
+    print("5. Exit.")
+
+
+def line_break():
+    print("\n")
